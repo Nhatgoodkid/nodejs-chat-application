5fa2224a3d36ce23a509f3c4af3d366bd8b1571c
unknown <dcn22402@gmail.com>
2023-04-06 15:49:23 +0700

    Creating inital components and adding React Router

--- frontend-mini-webchat/src/App.js
+++ frontend-mini-webchat/src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import Chat from './components/Chat/Chat';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Chat App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Chat} />
+          <Route path='/auth/login' component={Login} />
+          <Route path='/auth/register' component={Register} />
+          <Route render={() => <h1>404 Page not found</h1>} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
