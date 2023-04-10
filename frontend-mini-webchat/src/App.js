096ab31a13bbec618eabdbefc1229f8fb5535d71
unknown <dcn22402@gmail.com>
2023-04-11 02:00:05 +0700

    Adding protected route

--- frontend-mini-webchat/src/App.js
+++ frontend-mini-webchat/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import Chat from './components/Chat/Chat';
+import ProtectedRoute from './components/Router/ProtectedRoute';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
@@ -11,7 +12,7 @@ function App() {
         <Router>
             <div className="App">
                 <Switch>
-                    <Route exact path="/" component={Chat} />
+                    <ProtectedRoute exact path="/" component={Chat} />
                     <Route path="/auth/login" component={Login} />
                     <Route path="/auth/register" component={Register} />
                     <Route render={() => <h1>404 Page not found</h1>} />
