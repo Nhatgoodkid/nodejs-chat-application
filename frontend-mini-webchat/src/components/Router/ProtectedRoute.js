33d2ec1fd6e244f172ba0451a04f589273b45af0
unknown <dcn22402@gmail.com>
2023-04-11 02:14:36 +0700

    Creating chat Navbar with style

--- frontend-mini-webchat/src/components/Router/ProtectedRoute.js
+++ frontend-mini-webchat/src/components/Router/ProtectedRoute.js
@@ -1,19 +1,22 @@
-import React from "react"
-import {Route, Redirect} from 'react-router-dom'
-import { useSelector } from "react-redux"
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const ProtectedRoute = ({component: Component, ...props}) => {
-    const isLoggedIn = useSelector(state => state.authReducer.isLoggedIn)
+const ProtectedRoute = ({ component: Component, ...props }) => {
+    const isLoggedIn = useSelector((state) => state.authReducer.isLoggedIn);
 
-    return(
-        <Route {...props}
-            render={(props) =>(
-                isLoggedIn
-                    ? <Component {...props}/>
-                    : <Redirect to = '/auth/login'/>
-            )}
+    return (
+        <Route
+            {...props}
+            render={(props) =>
+                isLoggedIn ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to="/auth/login" />
+                )
+            }
         />
-    )
-}
+    );
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
