952f1a179c446665b0dbbfcefe432a300dc448b9
Dang Cong Nhat <dcn22402@gmail.com>
2023-04-10 13:40:16 +0700

    Creating Redux store

--- frontend-mini-webchat/src/store/actions/auth.js
+++ frontend-mini-webchat/src/store/actions/auth.js
@@ -1,10 +1,10 @@
-export const LOGIN = 'LOGIN'
 import AuthService from "../../services/authService"
+export const LOGIN = 'LOGIN'
 
 export const login = (params) => dispatch => {
     return AuthService.login(params)
     .then(data => {
-        console.log(res)
+        console.log(data)
         dispatch({type: LOGIN, payload: data})
     })
     .catch(err=>{
