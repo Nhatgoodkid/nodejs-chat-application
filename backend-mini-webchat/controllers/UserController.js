ce450d4b2036923a53bc34f1d8be307188f3552e
unknown <dcn22402@gmail.com>
2023-04-12 00:43:29 +0700

    Creating Update Controller

--- backend-mini-webchat/controllers/UserController.js
+++ backend-mini-webchat/controllers/UserController.js
@@ -2,8 +2,24 @@ const User = require('../models').User;
 const sequelize = require('sequelize');
 
 class UserController {
-    update(req, res) {
-        return res.send('Update');
+    async update(req, res) {
+        try {
+            const [rows, result] = await User.update(req.body, {
+                where: {
+                    id: req.user.id,
+                },
+                returning: true,
+                individualHooks: true,
+            });
+
+            const user = result[0].get({ raw: true });
+            user.avatar = result[0].avatar;
+            delete user.password;
+
+            return res.send(user);
+        } catch (e) {
+            return res.status(500).json({ error: e.message });
+        }
     }
 }
 module.exports = new UserController();
--- backend-mini-webchat/routes/user.js
+++ backend-mini-webchat/routes/user.js
@@ -3,7 +3,8 @@ const UserController = require('../controllers/UserController');
 const router = express.Router();
 const { validate } = require('../validators');
 const { auth } = require('../middleware/auth');
+const { rules: updateRules } = require('../validators/user/update');
 
-router.post('/update', [auth], UserController.update);
+router.post('/update', [auth, updateRules, validate], UserController.update);
 
 module.exports = router;
