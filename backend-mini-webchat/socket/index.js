2513f53b785195ea4d5d5f1da46a5e56e9de9995
unknown <dcn22402@gmail.com>
2023-04-13 18:26:00 +0700

    Adding socket on join event

--- backend-mini-webchat/socket/index.js
+++ backend-mini-webchat/socket/index.js
@@ -1,11 +1,50 @@
 const socketIo = require('socket.io');
 
+const users = new Map();
+
 const SocketServer = (server) => {
     const io = socketIo(server);
 
     io.on('connection', (socket) => {
         socket.on('join', async (user) => {
-            console.log('New user joined: ', user.firstName);
+            let sockets = [];
+
+            if (users.has(user.id)) {
+                const existingUser = users.get(user.id);
+                existingUser.sockets = [
+                    ...existingUser.sockets,
+                    ...[socket.id],
+                ];
+                users.set(user.id, existingUser);
+                sockets = [...existingUser.sockets, ...[socket.id]];
+            } else {
+                users.set(user.id, { id: user.id, sockets: [socket.id] });
+                sockets.push(socket.id);
+            }
+
+            const onlineFriends = []; //ids
+
+            const chatters = []; //query
+
+            // notify his friends that user is now online
+            for (let i = 0; i < chatters.length; i++) {
+                if (users.has(chatters[i].id)) {
+                    const chatter = users.get(chatters[i]);
+                    chatters.sockets.forEach((socket) => {
+                        try {
+                            io.to(socket).emit('online', user);
+                        } catch (e) {}
+                    });
+                    onlineFriends.push(chatter.id);
+                }
+            }
+
+            //send to user sockets which of this friends are online
+            socket.forEach((socket) => {
+                try {
+                    io.to(socket).emit('friends', onlineFriends);
+                } catch (e) {}
+            });
 
             io.to(socket.id).emit('typing', 'User typing ...');
         });
