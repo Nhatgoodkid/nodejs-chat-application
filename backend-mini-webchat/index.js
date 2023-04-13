189602f52a1cd5ba097458fa1a8f2fa5682804b1
unknown <dcn22402@gmail.com>
2023-04-13 16:33:10 +0700

    Adding sockets in Node

--- backend-mini-webchat/index.js
+++ backend-mini-webchat/index.js
@@ -4,6 +4,7 @@ const route = require('./routes');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const http = require('http');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,6 +14,11 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/uploads'));
 
 const port = config.appPort;
+
+const server = http.createServer(app);
+const SocketServer = require('./socket');
+SocketServer(server);
+
 app.listen(port, () => {
     console.log(`Server on listening on port ${config.appUrl}:${port}`);
 });
